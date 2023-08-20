1babb784a6d08d4821e4370c40f3c6b945d32a65
Darren Xu <darrenxu1994@gmail.com>
2023-08-20 10:26:27 +1000

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
+import * as packageJson from "./package.json";
+import tsConfigPaths from "vite-tsconfig-paths";
+import dts from "vite-plugin-dts";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    tsConfigPaths(),
+    dts({
+      include: ["src/components/"],
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve("src", "components/index.ts"),
+      name: "VueTable",
+      formats: ["es", "umd"],
+      fileName: (format) => `vue-table.${format}.js`,
+    },
+    // rollupOptions: {
+    //   external: [...Object.keys(packageJson.peerDependencies)],
+    // },
+  },
+});
